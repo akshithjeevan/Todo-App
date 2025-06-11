c6614270f2275f5aa665011141a787513668fe6a
akshithjeevan <akshithjeevan2000@gmail.com>
2025-06-11 13:42:28 +0530

    Add full Todo-App project

---
+++ src/App.js
@@ -0,0 +1,17 @@
+import logo from './logo.svg';
+import './App.css';
+import AddTodo from './components/addTodo/addTodo';
+import Todos from './components/todos/todos';
+
+
+function App() {
+  return (
+    <div className="App">
+<AddTodo/>
+<Todos/>
+
+    </div>
+  );
+}
+
+export default App;
